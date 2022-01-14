8497a5dd62c4ecccec1459d3ab879bb581eb52b9
shu-illy <sh.illy.9117@gmail.com>
2022-01-15 01:38:46 +0900

    4-3

---
+++ frontend/src/containers/Foods.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Foods = ({ match }) => {
+  return (
+    <>
+      フード一覧
+      <p>
+        restaurantsIdは {match.params.restaurantsId} です
+      </p>
+    </>
+  )
+}
